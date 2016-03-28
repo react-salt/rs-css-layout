8ffaafc064a430f6fb53142618e7caeeca5d6721
yanweiqing <yanweiqing@youzan.com>
2016-03-28 08:18:55 +0800

    补充样例代码

---
+++ examples/demo.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import '../assets/demo.less';
+
+export default class Demo extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <div className="row">
+                        <div className="layout-demo col-sm-2">col-sm-2</div>
+                        <div className="layout-demo col-sm-2">col-sm-2</div>
+                        <div className="layout-demo col-sm-2">col-sm-2</div>
+                        <div className="layout-demo col-sm-2">col-sm-2</div>
+                        <div className="layout-demo col-sm-2">col-sm-2</div>
+                        <div className="layout-demo col-sm-2">col-sm-2</div>
+                        <div className="layout-demo col-sm-2">col-sm-2</div>
+                        <div className="layout-demo col-sm-2">col-sm-2</div>
+                        <div className="layout-demo col-sm-2">col-sm-2</div>
+                        <div className="layout-demo col-sm-2">col-sm-2</div>
+                        <div className="layout-demo col-sm-2">col-sm-2</div>
+                        <div className="layout-demo col-sm-2">col-sm-2</div>
+                    </div>
+                </div>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="layout-demo col-sm-6">col-sm-6</div>
+                        <div className="layout-demo col-sm-12">col-sm-12</div>
+                        <div className="layout-demo col-sm-6">col-sm-6</div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
